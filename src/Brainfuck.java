ff5f74f678fa7a8371c9dc4b7149abf5f05b7bb1
Valeryi Tselishchev <var.tsel@gmail.com>
2018-02-09 23:20:07 +0300

    repair twoBrackets to newTwo  with old code

--- src/Brainfuck.java
+++ src/Brainfuck.java
@@ -1,5 +1,4 @@
 import java.util.*;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 
@@ -8,110 +7,133 @@ import java.util.regex.Pattern;
  */
 public class Brainfuck {
     public static HashMap<Integer, Integer> twoBrackets(String brainString) {
-        // making brackets pair [ - ] & ] - [
+        // making brackets isBracketsWell [ - ] for jump forward & ] - [ for jump backward
         // open brackets
-        ArrayList<Integer> oB = new ArrayList<>();
+        ArrayList<Integer> opnB = new ArrayList<>();
         // resulting pairs
         HashMap<Integer, Integer> result = new HashMap<>();
-        int pOfoB = brainString.indexOf("[");
-        // isn't open brackets
-        if (pOfoB < 0) return null;
-        oB.add(pOfoB);
-        int clsBrPos=0;
-        int opnBrPos=0;
+        int posOfoB = brainString.indexOf("[");
+        // there isn't open brackets
+        if (posOfoB < 0) return null;
+        opnB.add(posOfoB);
+        int opnBrPos = 0;
+        int clsBrPos = 0;
         boolean clsBrSearch = true;
+
         while (true) {
             if (clsBrSearch) clsBrPos = brainString.indexOf("]", ++clsBrPos);
             // close bracket not found - break
             if (clsBrPos < 0) break;
-            if (pOfoB >= 0) pOfoB = brainString.indexOf("[", ++pOfoB);
+
+            if (posOfoB >= 0) posOfoB = brainString.indexOf("[", ++posOfoB);
             // there isn't open brackets in brainString
-            if (pOfoB < 0) {
+            if (posOfoB < 0) {
                 clsBrSearch = true;
                 // adding pairs-positions [] & ][
-                result.put(oB.get(oB.size() - 1), clsBrPos);
-                result.put(clsBrPos, oB.get(oB.size() - 1));
-                oB.remove(oB.size() - 1);
-            } else if (pOfoB < clsBrPos) {
+                result.put(opnB.get(opnB.size() - 1), clsBrPos);
+                result.put(clsBrPos, opnB.get(opnB.size() - 1));
+                opnB.remove(opnB.size() - 1);
+            } else if (posOfoB < clsBrPos) {
                 clsBrSearch = false;
-                oB.add(pOfoB);
+                opnB.add(posOfoB);
             } else {
                 // there is open bracket after close.
                 clsBrSearch = true;
-                opnBrPos = oB.get(oB.size() - 1);
+                opnBrPos = opnB.get(opnB.size() - 1);
                 // adding pairs-positions with privious found [
                 result.put(opnBrPos, clsBrPos);
                 result.put(clsBrPos, opnBrPos);
-                oB.remove(oB.size() - 1);
-                oB.add(pOfoB);
+                opnB.remove(opnB.size() - 1);
+                opnB.add(posOfoB);
             }
         }
         return result;
     }
 
-    public static boolean pair(String brainString) {
-        // check the correctness of the brackets
-//        Pattern pattern = Pattern.compile("[^\\[\\]]");
-//        Matcher matcher = pattern.matcher(brainString);
-//        String result = Pattern.compile("[^\\[\\]]").matcher(brainString).replaceAll("");
-//        String result = pattern.matcher(brainString).replaceAll("");
-//        String result = matcher.replaceAll("");
-//  retrieval all brackets for valid analyses
-        char[] brck = Pattern.compile("[^\\[\\]]").matcher(brainString).replaceAll("").toCharArray();
-//        char[] brck = result.toCharArray();
+    private static Map<Integer, Integer> newTwo(String brainString) {
+//  retrieval all brackets from brainString for valid analyses
+        char[] brackets = Pattern.compile("[^\\[\\]]").matcher(brainString).replaceAll("").toCharArray();
         int brCnt = 0;
-        for (int i = 0; i < brck.length; i++) {
-            if (((brck[i] == '[')?brCnt++:brCnt--)<0) break;
-//            if ((brCnt=(brck[i] == '[')?brCnt++:brCnt--)<0) break;
-//            if (brck[i] == '[') brCnt++;
-//            else brCnt--;
-//            if (brCnt < 0) break;
+//      current brCnt must be non negative & at the end must be = 0
+        for (int i = 0; i < brackets.length; i++)
+            if (((brackets[i] == '[') ? brCnt++ : brCnt--) < 0) break;
+
+        if (brCnt < 0) throw new IllegalArgumentException("Wrong set of brackets");
+
+        Map<Integer, Integer> result = new HashMap<>();
+        if (brackets.length==0) return result;      //  empty result
+        Stack<Integer> opnBr = new Stack<>();
+        int brcktsPos=-1;
+        int tmp;
+//      brackets sequance is well here. It is possible make pairs [] & }{
+        for (int i = 0; i < brackets.length; i++) {
+            brcktsPos = brainString.indexOf(brackets[i], ++brcktsPos);
+            if (brackets[i] == '[')
+                opnBr.push(brcktsPos);
+            else {
+                tmp = opnBr.pop();
+                result.put(tmp, brcktsPos);
+                result.put(brcktsPos,tmp );
+            }
         }
+        return result;
+    }
+
+    private static boolean isBracketsWell(String brainString) {
+//  retrieval all brackets from brainString for valid analyses
+        char[] brck = Pattern.compile("[^\\[\\]]").matcher(brainString).replaceAll("").toCharArray();
+        int brCnt = 0;
+        for (int i = 0; i < brck.length; i++)
+//          current brCnt must be non negative & at the end must be = 0
+            if (((brck[i] == '[') ? brCnt++ : brCnt--) < 0) break;
         return brCnt == 0;
     }
 
     public static String brainfuck(String brainString) {
         String result = "";
-        if (!pair(brainString)) return result;
-        HashMap<Integer, Integer> bracketsPairs = twoBrackets(brainString);
+        if (!isBracketsWell(brainString)) return result;
+
+//        Map<Integer, Integer> bracketsPairs = twoBrackets(brainString);
+        Map<Integer, Integer> bracketsPairs = newTwo(brainString);
         char[] brainChars = brainString.toCharArray();
         int[] jobArray = new int[200];
-        int codPosition = 0, memoryPosition = 0;
+        int codPos = 0;
+        int memPos = 0;
         while (true) {
-            switch (brainChars[codPosition]) {
+            switch (brainChars[codPos]) {
                 case '>':
-                    memoryPosition++;
+                    memPos++;
                     break;
                 case '<':
-                    memoryPosition--;
+                    memPos--;
                     break;
                 case '+':
-                    jobArray[memoryPosition]++;
+                    jobArray[memPos]++;
                     break;
                 case '-':
-                    jobArray[memoryPosition]--;
+                    jobArray[memPos]--;
                     break;
                 case '.':
-                    result += Character.toString((char) jobArray[memoryPosition]);
+                    result += Character.toString((char) jobArray[memPos]);
                 case ',':
                     break; // here must be input
                 case '[':
-                    if (jobArray[memoryPosition] == 0) codPosition = bracketsPairs.get(codPosition);
+                    if (jobArray[memPos] == 0) codPos = bracketsPairs.get(codPos);
                     break;
                 case ']':
-                    if (jobArray[memoryPosition] != 0) codPosition = bracketsPairs.get(codPosition);
+                    if (jobArray[memPos] != 0) codPos = bracketsPairs.get(codPos);
                     break;
                 default:
             }
-            codPosition++;
-            if (codPosition >= brainChars.length) break;
+            codPos++;
+            if (codPos >= brainChars.length) break;
         }
         return result;
     }
 
     public static void main(String[] args) {
         String st = "++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.";
-        if (pair(st)) System.out.println(brainfuck(st));
+        if (isBracketsWell(st)) System.out.println(brainfuck(st));
 
     }
 }
